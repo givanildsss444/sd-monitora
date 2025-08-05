ada4917b26451ebd1dbb7bae324592d59c52e22f
givanildsss444 <cicerofarias1390gmil@gmail.com>
2025-08-05 15:45:35 -0300

    section contact add but imcomplete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -814,6 +814,63 @@ export default function Home() {
             </div>
           </section>
           
+          {/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/}
+
+          <section id="contact" className="relative w-full overflow-hidden py-12 md:py-24 lg:py-32 bg-white">
+            <div className="absolute bottom-0 left-1/2 z-0 -translate-x-1/2 transform">
+                <svg viewBox="0 0 959 454" className="w-[1000px] bg-[#DBF5FF] rotate-12 fill-[#DBF5F] blur-3xl"><path d="M893.499 20.6108C981.675 72.0768 976.157 235.045 897.35 323.727C818.543 412.41 665.883 425.5 546.664 438.892C428.199 451.737 342.484 464.813 234.275 438.76C126.066 412.708 -5.45475 348.692 0.802652 287.54C6.99684 227.005 150.279 169.262 246.36 136.65C342.379 104.656 391.26 97.1741 506.98 63.4508C622.701 29.7274 804.634 -30.9259 893.499 20.6108Z" fill="#DBF5FF"></path></svg>
+            </div>
+
+            <div className="container relative mx-auto px-4 md:grid-cols-2 md:px-6">
+              <div className="mx-auto grid max-w-6xl grid-cols-1 items-center gap-16 lg:grid-cols-2">
+                <div>
+                  <div className="space-y-2 text-center lg:text-left">
+                    <div className="inline-block rounded-lg bg-background/70 px-3 py-1 text-sm font-semibold text-primary">Contato</div>
+                    <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">Vamos conversar sobre como escalar o acompanhamento dos seus pacientes com obesidade?</h2>
+                    <p className="max-w-3xl text-foreground/80 md:text-xl/relaxed">Preencha o formulário abaixo para falar com um especialista do nosso time.</p>
+                    <p className="max-w-3xl text-foreground/80 md:text-xl/relaxed">Vamos entender sua realidade e te mostrar como o SD Monitora pode se integrar ao seu plano de forma simples, eficiente e lucrativa.</p>
+                  </div>
+
+                  <div className="mt-12">
+                    <ul className="w-full space-y-2 text-left">
+
+                      <li className="flex items-start gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-check-big mt-0.5 h-5 w-5 flex-shrink-0 text-[#40CDFB]"><path d="M21.801 10A10 10 0 1 1 17 3.335"></path><path d="m9 11 3 3L22 4"></path></svg>
+                        <div className="flex flex-col">
+                          <span>Conheça os benefícios do acompanhamento contínuo e automatizado via WhatsApp</span>
+                        </div>
+                      </li>
+
+                      <li className="flex items-start gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-check-big mt-0.5 h-5 w-5 flex-shrink-0 text-[#40CDFB]"><path d="M21.801 10A10 10 0 1 1 17 3.335"></path><path d="m9 11 3 3L22 4"></path></svg>
+                        <div className="flex flex-col">
+                          <span>30 dias gratuitos</span>
+                        </div>
+                      </li>
+
+                      <li className="flex items-start gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-check-big mt-0.5 h-5 w-5 flex-shrink-0 text-[#40CDFB]"><path d="M21.801 10A10 10 0 1 1 17 3.335"></path><path d="m9 11 3 3L22 4"></path></svg>
+                        <div className="flex flex-col">
+                          <span>30 dias gratuitos</span>
+                        </div>
+                      </li>
+
+                      <li className="flex items-start gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-circle-check-big mt-0.5 h-5 w-5 flex-shrink-0 text-[#40CDFB]"><path d="M21.801 10A10 10 0 1 1 17 3.335"></path><path d="m9 11 3 3L22 4"></path></svg>
+                        <div className="flex flex-col">
+                          <span>30 dias gratuitos</span>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+
+
+                </div>
+
+                <div></div>
+              </div>
+            </div>
+          </section>
 
         </div>
         <footer></footer>
